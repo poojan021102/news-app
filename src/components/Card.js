b8db95724f057ec1eb3f9312efa6fb41cd2184fe
Poojan Patel <poojanpatel02112002@gmail.com>
2022-08-20 01:08:10 +0530

    This is my third commit where I had changed the border of the card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from 'react'
 function Card({ImageSrc,Title,Description,NewsLink}) {
     return (
-        <div className="card">
+        <div className="card border border-success mb-2">
             <img src={ImageSrc?ImageSrc:"https://cdn.pixabay.com/photo/2015/02/15/09/33/news-636978_1280.jpg"} className="card-img-top" alt="News"/>
                 <div className="card-body">
                     <h5 className="card-title">{Title}</h5>
