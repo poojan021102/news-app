f2e8d556b4fd23d55ae5f19168e0fe02e9482df5
Poojan Patel <poojanpatel02112002@gmail.com>
2022-08-26 22:39:14 +0530

    Now I made the navigation theme dark

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg bg-light sticky-top">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">NewsApp</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
